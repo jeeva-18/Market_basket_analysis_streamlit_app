5f86f6d668fba61b71fc452c9c035c64cb2b213d
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-17 21:35:29 +0530

    Update MBA_App.py

--- MBA_App.py
+++ MBA_App.py
@@ -762,7 +762,7 @@ with col2:
                 "`MLxtend association_rules(), The function generates a DataFrame of association "
                 "rules including the metrics 'score', 'confidence', and 'lift'")
     with st.spinner("Generating the Frequent Itemsets and Assosiation Rules..."):
-        rules = pd.read_csv('new_rules.csv')
+        rules = pd.read_csv('new_rules (1).csv')
         rules.drop(coulmns=['Unamed:0'],inplace=True)
 
     """Assosiation Rules"""
