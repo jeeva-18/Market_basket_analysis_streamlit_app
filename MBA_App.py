cf7d1f01bebcdc09c4a865f93a36e127d284da95
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-17 19:31:48 +0530

    Update MBA_App.py

--- MBA_App.py
+++ MBA_App.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import plotly.express as px
 from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+import plotly.graph_objects as go 
 import warnings 
 
 import seaborn as sns
