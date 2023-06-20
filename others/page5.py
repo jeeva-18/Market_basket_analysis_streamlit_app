c4169881c0d251f3989faac65e42e97fe2a9dfd8
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-20 13:27:14 +0530

    Update page5.py

--- others/page5.py
+++ others/page5.py
@@ -31,11 +31,10 @@ from numpy.lib.arraysetops import unique
 
 
 st.markdown(" <h3 style='text-align: center;'>Product recomendation <i>(people who bought this also bought)</i>:</h3>", unsafe_allow_html=True)
+with st.spinner("Generating the Frequent Itemsets and Assosiation Rules..."):
+  product = pd.read_csv('product_rec.csv')
 
-product = pd.read_csv('product_rec.csv')
-
-
-# st.dataframe(product.head(10))
+st.dataframe(product.head(10))
 
 
 
