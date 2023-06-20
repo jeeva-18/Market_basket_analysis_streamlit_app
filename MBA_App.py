e66cbe83060a0b4ac610c1dd0be93cfcfd307576
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-20 20:15:07 +0530

    Update MBA_App.py

--- MBA_App.py
+++ MBA_App.py
@@ -73,8 +73,8 @@ col1, col2,col3 = st.columns((1,0.1,1))
 
 with col1:
    
-    st.markdown("### ***Project Contributor:***")
-    st.markdown("Santhosh Kumar.P")
+    st.markdown("### Project Contributor:")
+    st.markdown("##### Santhosh Kumar.P")
     st.markdown("MBA(Business Analytics)")
 
     st.markdown("### **Project Introduction**")
