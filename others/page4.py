2ce9f6fa594672ebee4354912f177b96f76c9114
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-20 11:22:11 +0530

    Update page4.py

--- others/page4.py
+++ others/page4.py
@@ -1,1 +1,6 @@
-
+'''
+### **Customer Segmentation**
+Customer segmentation is the process by which you divide your customers into segments up based on common characteristics – such as demographics or behaviors, so you can market to those customers more effectively <br><br>
+Customer segmentation is the process of dividing customers into groups based on common characteristics so companies can market to each group effectively and appropriately.
+* RFM (recency, frequency, monetary) Analysis
+'''
