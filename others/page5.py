ca4f555c9c2ba0a0c3f9a7e9b0e5ad51d68d62e3
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-20 13:23:19 +0530

    Update page5.py

--- others/page5.py
+++ others/page5.py
@@ -32,10 +32,10 @@ from numpy.lib.arraysetops import unique
 
 st.markdown(" <h3 style='text-align: center;'>Product recomendation <i>(people who bought this also bought)</i>:</h3>", unsafe_allow_html=True)
 
-product = pd.read_csv('product_rec.csv')
+# product = pd.read_csv('product_rec.csv')
 
 
-st.dataframe(product)
+# st.dataframe(product)
 
 
 
