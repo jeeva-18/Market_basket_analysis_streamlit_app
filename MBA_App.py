1ee3f1cd2bf451a8b4884bb6308da4b71eb2f4f4
JEEVA <113780413+jeeva-18@users.noreply.github.com>
2023-06-24 21:37:33 +0530

    Update MBA_App.py

--- MBA_App.py
+++ MBA_App.py
@@ -27,7 +27,7 @@ from mlxtend.frequent_patterns import association_rules
 from st_pages import show_pages_from_config, add_page_title
 
 # # Either this or add_indentation() MUST be called on each page in your
-# app to add indendation in the sidebar
+
 add_page_title()
 
 show_pages_from_config()
